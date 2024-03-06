a5ad63f39bbe8b10bdd795eb5a0d5324551230e6
Valen <gg172138406@gmail.com>
2024-03-06 16:17:48 +0800

    fix

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -5,7 +5,7 @@ const NewsBoard = ({category}) => {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    let url = `https://newsapi.org/v2/top-headlines?country=tw&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
     fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));
 
   },[category]);
@@ -16,7 +16,7 @@ const NewsBoard = ({category}) => {
         Business <span className="badge bg-danger">News</span>
       </h2>
       {articles.map((news,index)=>{
-        return <NewsItem key={index} title={news.title}  src={news.urlToImage} url={news.url}/>
+        return <NewsItem key={index} title={news.title} description={news.description}  src={news.urlToImage} url={news.url}/>
       })}
     </div>
   );
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,25 +1,25 @@
-import NewsInstead from '../assets/News.png'
+import newsImg from '../assets/News.png'
 import React from "react";
 
-const NewsItem = ({ title, description, url }) => {
+const NewsItem = ({ title,src, description, url }) => {
   return (
     <div
       className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2"
       style={{ maxWidth: "345px" }}
     >
       <img
-        src={NewsInstead}
+        src={src?src:newsImg}
         style={{ height: "200px", width: "326px" }}
         className="card-img-top"
         alt="..."
       />
       <div className="card-body">
         <h5 className="card-title">{title.slice(0, 50)}</h5>
-        {/* <p className="card-text">
+        <p className="card-text">
           {description
             ? description.slice(0, 90)
             : "news is a report of current event. It is information about something that has just happened."}
-        </p> */}
+        </p>
         <a href={url} className="btn btn-primary">
           Read more
         </a>
